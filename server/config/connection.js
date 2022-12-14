8d87861ec7db3c697f9d38c5d184c0282c773864
cayean <collinayala@gmail.com>
2022-12-13 22:48:01 -0600

    refactored connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project-3', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
