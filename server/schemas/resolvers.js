48843492010df02f382d16584dd453246e3da7a2
cayean <collinayala@gmail.com>
2022-12-14 20:39:52 -0600

    added initial prettier files and format all files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,56 +1,58 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express')
+const { User } = require('../models')
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
-  Query: {
-    user: async (parent, args, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
-        });
+    Query: {
+        user: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user._id).populate({
+                    path: 'orders.products',
+                    populate: 'category',
+                })
 
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+                user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate)
 
-        return user;
-      }
+                return user
+            }
 
-      throw new AuthenticationError('Not logged in');
-    }
-  },
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
+            throw new AuthenticationError('Not logged in')
+        },
     },
-    updateUser: async (parent, args, context) => {
-      if (context.user) {
-        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-      }
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args)
+            const token = signToken(user)
 
-      throw new AuthenticationError('Not logged in');
+            return { token, user }
+        },
+        updateUser: async (parent, args, context) => {
+            if (context.user) {
+                return await User.findByIdAndUpdate(context.user._id, args, {
+                    new: true,
+                })
+            }
+
+            throw new AuthenticationError('Not logged in')
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const correctPw = await user.isCorrectPassword(password)
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const token = signToken(user)
+
+            return { token, user }
+        },
     },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+}
 
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    }
-  }
-};
-
-module.exports = resolvers;
+module.exports = resolvers
