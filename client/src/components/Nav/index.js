17cb2c3d31c8a9b4fe63b187d4e577d5887d529f
TomLuna <lomtuna15@gmail.com>
2022-12-18 17:53:13 -0500

    updated merge

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -58,7 +58,6 @@ function Navi() {
       );
     }
 
-<<<<<<< HEAD
   return (
     
     <Navbar className="navb">
@@ -77,21 +76,3 @@ function Navi() {
 }
 
 export default Navi;
-=======
-    return (
-        <header className="flex-row px-1">
-            <h1>
-                <Link to="/">
-                    <span role="img" aria-label="shopping bag">
-                        Path-finder 🔭
-                    </span>
-                </Link>
-            </h1>
-
-            <nav>{showNavigation()}</nav>
-        </header>
-    )
-}
-
-export default Nav
->>>>>>> develop
