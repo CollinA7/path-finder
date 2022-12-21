60d91347bfc4bfb99b2c173c6e8574642d79da6f
cayean <collinayala@gmail.com>
2022-12-20 22:26:08 -0600

    added onClick functionality for the park cards. weather API returns current weather conditions

--- client/src/components/ParkList/index.js
+++ client/src/components/ParkList/index.js
@@ -1,19 +1,27 @@
-import React, { useEffect, useState } from 'react'; 
-import Button from 'react-bootstrap/Button'; 
-import Card from 'react-bootstrap/Card'; 
+import React, { useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 
 function ParkList() {
     const state = 'IL'
     const [parks, setParks] = useState([])
 
-    function formHandler(data) {}
-    // function weatherApi() {
-    //     fetch(
-    //         `http://api.openweathermap.org/geo/1.0/direct?q=sanantonio,country=US&limit=5&appid=4fce90cadf9eba29dd340628ab5664dc`
-    //     )
-    //         .then((response) => response.json())
-    //         .then((data) => console.log(data))
-    // }
+    function checkWeather(park) {
+        const lat = park.latitude
+        const lon = park.longitude
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=4fce90cadf9eba29dd340628ab5664dc`
+        )
+            .then((response) => response.json())
+            // This console.log responds with the weather data on the clicked park
+            .then((data) => console.log(data))
+    }
+
+    // weather click handler passes the park data to the weather API
+    function weatherHandler(park, e) {
+        e.preventDefault()
+        checkWeather(park)
+    }
 
     useEffect(() => {
         fetch(
@@ -30,36 +38,39 @@ function ParkList() {
             })
     }, [])
 
-    function checkWeather() {}
-
     return (
         <div>
             {/* parks.map creates each list element from the nps API */}
             {parks.map((park) => {
                 return (
-                    
                     <div className="park-card" key={park.id}>
                         <div>
-                        <Card style={{ width: '18rem' }} >
-                          <Card.Img variant="top" src={park.images[0].url}></Card.Img>
-                        <Card.Body>    
-                            <Card.Title >{park.title}</Card.Title>
-                            <Card.Text>
-                            <strong>Ameneties:</strong>
-                            {park.amenities}
-                            </Card.Text>
-                        
-                        {/* The check weather button pings the open weather API to check the weather */}
-                        <Button variant="primary" className="weather-button"
-                            onClick={checkWeather()}>
-                            <strong>Check Weather</strong>
-                        </Button>
-                        <Button className="add-favorites"> 
-                            <strong>Add to Favorites</strong>
-                        </Button> 
-                        </Card.Body>  
-                        </Card>
-                        </div>  
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img
+                                    variant="top"
+                                    src={park.images[0].url}
+                                ></Card.Img>
+                                <Card.Body>
+                                    <Card.Title>{park.title}</Card.Title>
+                                    <Card.Text>
+                                        <strong>Ameneties:</strong>
+                                        {park.amenities}
+                                    </Card.Text>
+
+                                    {/* The check weather button pings the open weather API to check the weather */}
+                                    <Button
+                                        variant="primary"
+                                        className="weather-button"
+                                        onClick={(e) => weatherHandler(park, e)}
+                                    >
+                                        <strong>Check Weather</strong>
+                                    </Button>
+                                    <Button className="add-favorites">
+                                        <strong>Add to Favorites</strong>
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
                     </div>
                 )
             })}
