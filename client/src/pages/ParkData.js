83fd5bc5af7b22d14a2103471d02dc649ac03b25
cayean <collinayala@gmail.com>
2022-12-21 17:50:28 -0600

    merging collin to develop

--- client/src/pages/ParkData.js
+++ client/src/pages/ParkData.js
@@ -1,28 +1,97 @@
-import React from 'react';
-import ParkList from '../components/ParkList';
-import ButtonGroup from 'react-bootstrap/esm/ButtonGroup'; 
-import Dropdown from 'react-bootstrap/Dropdown'; 
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
-import Button from 'react-bootstrap/esm/Button';
+import React from 'react'
+import ParkList from '../components/ParkList'
+import ButtonGroup from 'react-bootstrap/esm/ButtonGroup'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import DropdownToggle from 'react-bootstrap/esm/DropdownToggle'
+import Button from 'react-bootstrap/esm/Button'
 
 const ParkData = () => {
+    const states = [
+        'AL',
+        'AK',
+        'AS',
+        'AZ',
+        'AR',
+        'CA',
+        'CO',
+        'CT',
+        'DE',
+        'DC',
+        'FM',
+        'FL',
+        'GA',
+        'GU',
+        'HI',
+        'ID',
+        'IL',
+        'IN',
+        'IA',
+        'KS',
+        'KY',
+        'LA',
+        'ME',
+        'MH',
+        'MD',
+        'MA',
+        'MI',
+        'MN',
+        'MS',
+        'MO',
+        'MT',
+        'NE',
+        'NV',
+        'NH',
+        'NJ',
+        'NM',
+        'NY',
+        'NC',
+        'ND',
+        'MP',
+        'OH',
+        'OK',
+        'PW',
+        'PA',
+        'PR',
+        'RI',
+        'SC',
+        'SD',
+        'TN',
+        'TX',
+        'UT',
+        'VT',
+        'VI',
+        'VA',
+        'WA',
+        'WV',
+        'WI',
+        'WY',
+    ]
+
+    const selectedState = states.map((state) => state)
+    console.log(selectedState)
+
     return (
         <div className="container">
             <div>
                 {/* TODO: add form data here */}
-                <Dropdown></Dropdown>
                 <form>
                     <Dropdown>
-                    <label>
-                        State (Two Letter Abbreviation):
-                        
-                        <Dropdown.Toggle variant="warning" id="dropdown-basic">Select a State</Dropdown.Toggle>
-                    {/* Add the ability to select from an array of state Abbreviations */}
-                    </label>
-                    <Button type="submit" value="Submit" >Go!</Button>
-                    
-                    <ParkList />
+                        <label>
+                            State (Two Letter Abbreviation):
+                            <Dropdown.Toggle
+                                variant="warning"
+                                id="dropdown-basic"
+                            >
+                                Select a State
+                            </Dropdown.Toggle>
+                            {/* Add the ability to select from an array of state Abbreviations */}
+                        </label>
+                        <Button type="submit" value="Submit">
+                            Go!
+                        </Button>
+
+                        {/* <ParkList /> */}
                     </Dropdown>
                 </form>
             </div>
